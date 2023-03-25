291b349473dee86e954ea69425efffcefc5ba74c
jinxcodex00 <122253506+jinxcodex00@users.noreply.github.com>
2023-03-25 03:53:29 -0500

    CampsitesDirectoryPage and CampsiteDetail

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -7,22 +7,24 @@ const CampsitesDirectoryPage = () => {
   let selectedCampsite = selectRandomCampsite();
 
   const toggleCampsite = () => {
-    selectedCampsite = selectRandomCampsite();
-    console.log(selectedCampsite);
+      selectedCampsite = selectRandomCampsite();
+      console.log(selectedCampsite);
   };
 
   return (
-    <Container>
-      <Button onClick={() => toggleCampsite()}>Select Random Campsite</Button>
-      <Row>
-        <Col sm="5" md="7">
-          <CampsitesList />
-        </Col>
-        <Col sm="7" md="5">
-          <CampsiteDetail campsite={selectedCampsite} />
-        </Col>
-      </Row>
-    </Container>
+      <Container>
+          <Button onClick={() => toggleCampsite()}>
+              Select Random Campsite
+          </Button>
+          <Row>
+              <Col sm='5' md='7'>
+                  <CampsitesList />
+              </Col>
+              <Col sm='7' md='5'>
+                  <CampsiteDetail campsite={selectedCampsite} />
+              </Col>
+          </Row>
+      </Container>
   );
 };
 
