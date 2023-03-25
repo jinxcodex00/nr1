d2f061c23299b123b5b9e3ac63ed7489aa21546b
jinxcodex00 <122253506+jinxcodex00@users.noreply.github.com>
2023-03-25 04:41:03 -0500

    useState

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,30 +1,24 @@
-import { Container, Row, Col, Button } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import CampsiteDetail from "../features/campsites/CampsiteDetail";
 import CampsitesList from "../features/campsites/CampsitesList";
-import { selectRandomCampsite } from "../features/campsites/campsitesSlice";
+import { useState } from "react";
+import { selectCampsiteById } from "../features/campsites/campsitesSlice";
 
 const CampsitesDirectoryPage = () => {
-  let selectedCampsite = selectRandomCampsite();
-
-  const toggleCampsite = () => {
-      selectedCampsite = selectRandomCampsite();
-      console.log(selectedCampsite);
-  };
+  const [campsiteId, setCampsiteId] = useState(0);
+  const selectedCampsite = selectCampsiteById(campsiteId);
 
   return (
-      <Container>
-          <Button onClick={() => toggleCampsite()}>
-              Select Random Campsite
-          </Button>
-          <Row>
-              <Col sm='5' md='7'>
-                  <CampsitesList />
-              </Col>
-              <Col sm='7' md='5'>
-                  <CampsiteDetail campsite={selectedCampsite} />
-              </Col>
-          </Row>
-      </Container>
+    <Container>
+      <Row>
+        <Col sm="5" md="7">
+        <CampsitesList setCampsiteId={setCampsiteId} />
+        </Col>
+        <Col sm="7" md="5">
+          <CampsiteDetail campsite={selectedCampsite} />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
