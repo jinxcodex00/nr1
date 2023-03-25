dd329b5aba0d2dd9066e4ef2da9577137e4e4889
jinxcodex00 <122253506+jinxcodex00@users.noreply.github.com>
2023-03-24 19:00:21 -0500

    Test changes #2 - footer

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CampsitesDirectoryPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
