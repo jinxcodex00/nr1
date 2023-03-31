e41565143ee85b9d203229963a36c52ce705591d
jinxcodex00 <122253506+jinxcodex00@users.noreply.github.com>
2023-03-31 18:16:55 -0500

    AnimatedDisplayCard

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,20 +1,27 @@
 import { Col, Row } from "reactstrap";
-import DisplayCard from "./DisplayCard";
+//import DisplayCard from "./DisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
 import { selectFeaturedPartner } from "../partners/partnersSlice";
+import AnimatedDisplayCard from "./AnimatedDisplayCard";
 
 const DisplayList = () => {
-  const items = [selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartner()];
+  const items = [
+    selectFeaturedCampsite(),
+    selectFeaturedPromotion(),
+    selectFeaturedPartner(),
+  ];
 
   return (
     <Row>
       {items.map((item, idx) => {
         return (
-          <Col md className="m-1" key={idx}>
-            <DisplayCard item={item} />
-          </Col>
-        );
+          item && (
+              <Col md className='m-1' key={idx}>
+                  <AnimatedDisplayCard item={item} />
+              </Col>
+        )
+      );
       })}
     </Row>
   );
