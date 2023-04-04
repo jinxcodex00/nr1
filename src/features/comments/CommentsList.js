70d28d475fda22f738def852328034ad91b8d182
jinxcodex00 <122253506+jinxcodex00@users.noreply.github.com>
2023-04-04 00:25:46 -0500

    Wk 3 Workshop Assignment (at-home fixes) - needs revision

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,12 +1,8 @@
 import { Col } from "reactstrap";
 import Comment from "./Comment";
 import { selectCommentsByCampsiteId } from "./commentsSlice";
-<<<<<<< HEAD
 import { CommentForm } from "./CommentForm";
 
-=======
-import { CommentsList } from './CommentsList';
->>>>>>> d62689038db221dd41599f793c50dc6840e2bc63
 
 const CommentsList = ({ campsiteId }) => {
   const comments = selectCommentsByCampsiteId(campsiteId);
@@ -15,17 +11,9 @@ const CommentsList = ({ campsiteId }) => {
       <Col md="5" className="m-1">
         <h4>Comments</h4>
         {comments.map((comment) => {
-<<<<<<< HEAD
             return <Comment key={comment.id} comment={comment} />;
-=======
-          return <Comment key={comment.id} comment={comment} />;
-        <CommentsList />;
-        })}
->>>>>>> d62689038db221dd41599f793c50dc6840e2bc63
       </Col>
-    );
-  }
-};
+    )}};
 
 <CommentForm campsiteId = {campsiteId} />
 
