664937e9f0b9ef77027e4b9a571f7e9658cceab5
jinxcodex00 <122253506+jinxcodex00@users.noreply.github.com>
2023-04-07 18:25:50 -0500

    Campsites useSelector - Fixed vers

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,25 +1,16 @@
-import createSlice from "@reduxjs/toolkit";
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
-import { PROMOTIONS } from "../../app/shared/PROMOTIONS";
-
-export const selectFeaturedPromotion = () => {
-  return PROMOTIONS.find((promotion) => promotion.featured);
-};
+import { createSlice } from '@reduxjs/toolkit';
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
 
 const initialState = {
-  campsitesArray: CAMPSITES,
+    campsitesArray: CAMPSITES
 };
 
 const campsitesSlice = createSlice({
-  name: "campsites",
-  initialState,
+    name: 'campsites',
+    initialState
 });
 
 export const campsitesReducer = campsitesSlice.reducer;
-
-export const selectAllCampsites = (state) => {
-  return state.campsites.campsitesArray;
-};
 
 export const selectCampsiteById = (id) => (state) => {
   return state.campsites.campsitesArray.find(
@@ -27,6 +18,9 @@ export const selectCampsiteById = (id) => (state) => {
   );
 };
 
+export const selectAllCampsites = (state) => {
+  return state.campsites.campsitesArray;
+};
 export const selectFeaturedCampsite = (state) => {
   return state.campsites.campsitesArray.find((campsite) => campsite.featured);
 };
