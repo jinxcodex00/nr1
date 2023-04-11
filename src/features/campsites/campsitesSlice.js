9ff64547709f70fdea393e8ad8020fe9f7f696ad
jinxcodex00 <122253506+jinxcodex00@users.noreply.github.com>
2023-04-11 12:31:22 -0500

    Redux campsitesSlice v2

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,22 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
-import { PROMOTIONS } from '../../app/shared/PROMOTIONS';
 
-export const selectFeaturedPromotion = () => {
-  return PROMOTIONS.find((promotion) => promotion.featured);
+const initialState = {
+    campsitesArray: CAMPSITES
 };
 
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
+
 export const selectAllCampsites = () => {
-  return CAMPSITES;
+    return CAMPSITES;
 };
 
 export const selectCampsiteById = (id) => {
-  return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
+    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
-//export const selectRandomCampsite = () => {
-  //return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
-//};
-
 export const selectFeaturedCampsite = () => {
-  return CAMPSITES.find((campsite) => campsite.featured);
+    return CAMPSITES.find((campsite) => campsite.featured);
 };
