04c1079efcedf42523a8274b84714bc7678cb3a4
jinxcodex00 <122253506+jinxcodex00@users.noreply.github.com>
2023-04-14 08:29:48 -0500

    Async Campsites

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchCampsites } from "./features/campsites/campsitesSlice";
 
 function App() {
+    const dispatch = useDispatch();
+
+useEffect(() => {
+    dispatch(fetchCampsites());
+}, [dispatch]);
     return (
         <div className='App'>
             <Header />
